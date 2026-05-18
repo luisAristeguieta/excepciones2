9ec27a430b6fa0acbb7a805633b83fdb5332b809
luisjimenezgx@gmai.com <luisjimenezgx@gmail.com>
2026-05-18 18:12:03 -0400

    first commit

---
+++ src/main/java/com/krakeDev/Taller/Exepciones2/Exepciones2Application.java
@@ -0,0 +1,13 @@
+package com.krakeDev.Taller.Exepciones2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exepciones2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exepciones2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakeDev/Taller/Exepciones2/Exepciones2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakeDev.Taller.Exepciones2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exepciones2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
